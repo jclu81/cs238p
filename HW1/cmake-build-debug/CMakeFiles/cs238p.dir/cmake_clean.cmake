36e847099e690c4ad060a965d3f23ee1b1f735fc
jclu81@gmail.com <jclu81@gmail.com>
2018-03-03 18:42:35 -0800

    hw3

---
+++ HW1/cmake-build-debug/CMakeFiles/cs238p.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs238p.dir/main.c.o"
+  "cs238p.pdb"
+  "cs238p"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cs238p.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW1/cmake-build-debug/CMakeFiles/cs238p.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cs238p.
+# This may be replaced when dependencies are built.
